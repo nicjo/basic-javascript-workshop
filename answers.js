232d530c44e24b6db0add3beaacc0e2247770647
nicjo <nicjorgensen@yahoo.ca>
2016-01-27 20:39:58 +0000

    Add 2 numbers

--- answers.js
+++ answers.js
@@ -3,9 +3,7 @@
 
 // Returns the first character of a string
 function firstCharacter(rString) {
-    var result = rString.charAt(0);
-    console.log(result);
-    return(result);
+    return(rString.charAt(0));
 }
 
 firstCharacter("Example")
@@ -14,9 +12,7 @@ firstCharacter("Example")
 // Returns the last charcter of a string
 function lastCharacter(randomString) {
     var last = randomString.length - 1;
-    var result = randomString.charAt(last);
-    console.log(result);
-    return(result);
+    return(randomString.charAt(last));
 }
 
 lastCharacter("Example")
@@ -24,19 +20,33 @@ lastCharacter("Example")
 
 //String and a number that returns the character at the position represented by the number.
 function numCharacter(randomString, number) {
-    var result = randomString.charAt(number);
-    console.log(result)
+    return(randomString.charAt(number));
 }
 
 numCharacter("Example",4)
 
+function numCharacter(randomString, number) {
+    return(randomString.charAt(number));
+}
+
 
 // Add two numbers together. When its not a number, the function will combine the inputs
 // example: ("twelve",12) returns "twelve12"
+
 function add(number1, number2) {
-    console.log(number1 + number2)
-    return(number1 + number2)
+    return(number1 + number2);
 }
 
 add(12,30)
 
+
+// Multiply two numbers together.
+// If I use a string and a number (or two strings), the result will be NaN
+function multiply(number1, number2) {
+    return(number1 * number2);
+}
+
+multiply(12,30)
+
+
+// Two numbers and a string
