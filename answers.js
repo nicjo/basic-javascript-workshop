718a889294f8c32bf616b970a4d878249510b1e3
nicjo <nicjorgensen@yahoo.ca>
2016-01-27 19:37:59 +0000

    Last character function

--- answers.js
+++ answers.js
@@ -1,9 +1,19 @@
 // I'm all about one-liners
 
 function firstCharacter(rString) {
-    var result = rString.charAt(0)
-    console.log(result)
-    return(result)
+    var result = rString.charAt(0);
+    console.log(result);
+    return(result);
 }
 
 firstCharacter("Example")
+
+
+function lastCharacter(randomString) {
+    var last = randomString.length - 1;
+    var result = randomString.charAt(last);
+    console.log(result);
+    return(result);
+}
+
+lastCharacter("Example")
